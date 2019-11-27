8218849de9d9fcae3510c8d0fb1016b76c1addab
marielysis <marielys11@yahoo.com>
2019-11-26 23:28:20 -0300

    Leyendo archivo md, primera subida

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+const path = require('path');
+
+
+readmeMD = fs.readFile('./README.md', 'utf-8', (Error, Data) => {
+    if (Error) {
+        console.log(Error);
+    } else(Data)
+    console.log(Data);
+
+
+});
